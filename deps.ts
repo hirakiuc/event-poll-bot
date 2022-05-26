16edb85ee7fbbfc71df763c7627f6cd04dfcb073
hirakiuc <hirakiuc@gmail.com>
2022-05-26 16:16:21 +0900

    Remove unused import

--- deps.ts
+++ deps.ts
@@ -35,5 +35,3 @@ export {
 } from "https://deno.land/x/sift@0.4.2/mod.ts";
 
 export type { Handler } from "https://deno.land/x/sift@0.4.2/mod.ts";
-
-export { decode } from "https://deno.land/std/encoding/base64url.ts";
