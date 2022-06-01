ece79701f519087adcc4930d71e61ce7139be8e1
hirakiuc <hirakiuc@gmail.com>
2022-06-02 08:41:31 +0900

    Remove unused handler

--- src/event/handlers/mod.ts
+++ src/event/handlers/mod.ts
@@ -1,12 +1,10 @@
 // handler generators
 import { guildCreateHandler } from "./guildCreate.ts";
-import { interactionCreateHandler } from "./interactionCreate.ts";
 import { messageCreateHandler } from "./messageCreate.ts";
 import { readyHandler } from "./ready.ts";
 
 export {
   guildCreateHandler,
-  interactionCreateHandler,
   messageCreateHandler,
   readyHandler,
 };
